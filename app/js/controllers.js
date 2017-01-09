c3a6c737d01099bfe5fea24c9e125cd3c8bc6237
Vladys <vladis.kh@gmail.com>
2017-01-09 12:28:29 +0200

    menu_added

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,13 +2,28 @@
 app.controller("MainController", [ '$scope', '$http', '$location', function ($scope, $http, $location) {
 	
 	$scope.description = "Season 2016/2017";
+	$scope.bg = '/img/background/02.jpg';
+
 
 	var date = new Date();
 	$scope.today = date;
 	$http.get('clubs/clubs.json').success(function(data){
 			$scope.infoClub = data;
 	});
+
+	
 }]);
+
+app.directive('backImg', function(){
+	return function(scope, element, attrs){
+		var url = attrs.backImg;
+		element.css({
+			'background-image': 'url(' + url +')',
+			'background-size' : 'cover'
+		});
+	};
+});
+
 
 app.filter('euroCups', function(){
 	return function(input){
@@ -18,18 +33,18 @@ app.filter('euroCups', function(){
 
 
 app.controller("SecondController", ['$scope', '$http', '$location', function ($scope, $http, $location) {
-    $scope.msg = "List";
+    $scope.msg = "News";
 }]);
 
 
 app.controller("ThirdController", ['$scope', '$http', '$location', function ($scope, $http, $location) {
-    $scope.msg = "News";
+    $scope.msg = "Video";
 }]);
 
 
 app.controller("InfoController", ['$scope', '$routeParams', function ($scope, $routeParams) {
 
-    $scope.msg = "Info";
+    $scope.msg = "Schedule";
     // $scope.f = $scope.infoClub[$routeParams.id]
 
 }]);
